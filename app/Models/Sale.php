9ab178225b26df3e4ab99c477e5e1d2da1c57723
IsaPuerta16 <isabelapuertaperez@gmail.com>
2024-12-07 18:25:30 -0500

    Configuracion del modelo Sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Sale extends Model
 {
-    //
+     //
+     use HasFactory;
+
+
+     protected $fillable = [
+        'total',
+        'created_at_exact',
+        'employed_id',
+        'dishes'
+     ];
 }
